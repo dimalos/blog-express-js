cd3e52c732f129b324183be718bfc7729e0b6dc8
dmitrylos <dima.l@tchdm.live>
2023-03-30 15:01:21 +0300

    Configured serving static files from the ./public directory

--- src/index.js
+++ src/index.js
@@ -4,8 +4,25 @@ const express = require('express');
 const app = express();
 const port = process.env.NODE_DOCKER_PORT || 8080;
 
+const path = require('path');
+const staticPath = path.join(__dirname, '/../public');
+console.log(staticPath);
+app.use(express.static(staticPath));
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  const responseContent = `
+  <html>
+  <body>
+  <h1>Hello, it's Dima's blog, be careful!</h1>
+  <div>
+  <img src="/img/funny-moose.png">
+  </div>
+</div>
+</body>
+  </html>
+  
+  `;
+  res.send(responseContent);
 })
 
 app.listen(port, () => {
