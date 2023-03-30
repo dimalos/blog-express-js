7ac8f6b31ec99f90983d33ec8f9291e35c812da1
dmitrylos <dima.l@tchdm.live>
2023-03-30 14:40:30 +0300

    First good-working docker setup

--- Makefile
+++ Makefile
@@ -1,5 +1,16 @@
+app: down up install run
+
 up:
-	docker-compose -f .docker/docker-compose.yml up -d --build --force-recreate
+	docker-compose -f .docker/docker-compose.yml up -d --build
+
+down:
+	docker-compose -f .docker/docker-compose.yml down
 
 build:
 	docker-compose -f .docker/docker-compose.yml build --no-cache
+
+install:
+	docker exec -t dimalos-blog-app bash -c 'npm install'
+
+run:
+	docker exec -t dimalos-blog-app bash -c 'npm run dev'
---
+++ src/index.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to DimaLos blog application. 5" });
+});
+
+// set port, listen for requests
+const PORT = process.env.NODE_DOCKER_PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
